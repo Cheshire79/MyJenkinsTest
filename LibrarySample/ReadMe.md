0efc2e88665be402873a116200cf464123f6efed
PavlovOV <cheshirelk@gmail.com>
2022-10-01 23:03:42 +0300

    Unitial commit

---
+++ LibrarySample/ReadMe.md
@@ -0,0 +1,3 @@
+The Jenkins Sample
+
+This is a sample project to demonstrate building a C# project in Jenkins and running tests.
---
+++ LibrarySample/Sample.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sample.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sample.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08ee31a7-7c4b-455a-92aa-46387f57d6ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
