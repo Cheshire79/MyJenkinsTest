5f22517fc9a49e0b53fe60560a63da44cf83ef01
PavlovOV <cheshirelk@gmail.com>
2022-10-02 21:38:16 +0300

    test

--- LibrarySample/Jenkinsfile.groovy
+++ LibrarySample/Jenkinsfile.groovy
@@ -16,7 +16,7 @@ pipeline {
         pollSCM 'H * * * *'
     }
     stages {
-        stage('This is a new 3 test!') {
+        stage('This is a new 4 test!') {
             steps {
                 script {
                     print 'Hello good World!'
